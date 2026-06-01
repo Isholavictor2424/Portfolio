e0fe086efb79527bee357fd791a8c8c449dd013b
Your Name <youremail@example.com>
2026-06-01 14:02:07 +0100

    Fix Vector.png case sensitivity

--- src/component/images.jsx
+++ src/component/images.jsx
@@ -4,7 +4,7 @@ import react from "../assets/react.svg";
 import reactnative from "../assets/react.svg";
 import css from "../assets/css.svg";
 import js from "../assets/js.svg";
-import vector from "../assets/vector.png";
+import vector from "../assets/Vector.png";
 import bootstrap from "../assets/bootstrap.svg";
 import typescript from "../assets/Group.png";
 import tailwind from "../assets/tailwind.png";
@@ -12,7 +12,7 @@ import tailwind from "../assets/tailwind.png";
 const images = [
   { src: git, name: "Git" },
   { src: react, name: "React" },
-   { src: reactnative, name: "React Native" },
+  { src: reactnative, name: "React Native" },
   { src: css, name: "CSS" },
   { src: html, name: "HTML" },
   { src: js, name: "JavaScript" },
@@ -20,7 +20,7 @@ const images = [
   { src: bootstrap, name: "Bootstrap" },
   { src: typescript, name: "TypeScript" },
   { src: tailwind, name: "Tailwind CSS" },
- 
+
 ];
 
 export default images;
